eddae3f8942f87b981304c0f4019dc7de6f54176
India Hiring <indiahiring@global.publicisgroupe.net>
2019-09-25 16:07:27 +0530

    test commit

--- src/test/java/com/football/footdemo/FootDemoApplicationTests.java
+++ src/test/java/com/football/footdemo/FootDemoApplicationTests.java
@@ -2,15 +2,40 @@ package com.football.footdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest
 public class FootDemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testGetTeamStandings() {
+		
+		RequestBuilder request = get("/standings/148");
+		
+        try {
+			this.mockMvc.perform(get("/standings/148"))
+			.andExpect(status().isOk());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
